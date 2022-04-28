a26a3193f1770c458e9fcc2c069ffa79a9674a01
BHemert <bjornvanhemert@gmail.com>
2022-04-28 23:59:22 +0200

    Checkpoint 2

--- TINBES02/src/main.cpp
+++ TINBES02/src/main.cpp
@@ -3,9 +3,30 @@
 const int BUFFER_SIZE = 12;
 char buf[BUFFER_SIZE];
 
+bool input_routine()
+{
+  // read if any data is available
+  if (Serial.available() > 0)
+  {
+    // read the length of incoming bytes:
+    int bufferLen = Serial.readBytes(buf, BUFFER_SIZE);
+    Serial.print("I received: ");
+    for (int i = 0; i < bufferLen; i++)
+    {
+      if (buf[i] == ' ' || buf[i] == '\n')
+      {
+        // return true en ga door naar functie
+        buf[i] = '\0';
+        return true;
+      }
+    }
+    // Complete buffer
+    Serial.println(buf);
+  }
+}
 void store()
 {
-  Serial.println("store");
+  Serial.println("Hij is in store");
 }
 
 typedef struct
@@ -21,28 +42,29 @@ static commandType command[] =
 
 void setup()
 {
+  input_routine() == false;
   Serial.begin(9600);
-  command[0].func();
+  // command[0].func();
   Serial.println(F("Arduino OS 1.0 Ready"));
 }
 
 void loop()
 {
-  // read if any data is available
-  if (Serial.available() > 0)
+  if (input_routine() == true)
   {
-    // read the length of incoming bytes:
-    int bufferLen = Serial.readBytes(buf, BUFFER_SIZE);
-    Serial.print("I received: ");
-    for (int i = 0; i < bufferLen; i++)
+    for (int i = 0; i < sizeof(command) / sizeof(commandType); i++)
     {
-      if (buf[i] == ' ' || buf[i] == '\n')
+      if (strcmp(command[i].name, buf) == 0)
       {
-        // return true en ga door naar functie
-        buf[i] = '\0';
+        Serial.println(F("Executing command"));
+        Serial.print(strcmp(command[i].name, buf));
+        command[i].func();
       }
     }
-    // Complete buffer
-    Serial.println(buf);
+  }
+  else
+
+  {
+    Serial.println(F("Command not found"));
   }
 }
